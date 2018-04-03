bf1982d436c99f010dc0d356db8ce309482a189f
Enoch Tsang <etsang1@hotmail.com>
2018-04-02 18:32:30 -0600

    lint board

--- hamilton_launch_board/src/main.js
+++ hamilton_launch_board/src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'vuetify/dist/vuetify.min.css';
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
