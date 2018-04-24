7d6f8658a18d22236a9c81bc501e84cbd9f80b9e
Byron Seto <byronseto@hotmail.com>
2018-04-23 22:59:02 -0600

    trying to add the video stream

---
+++ hamilton_launch_board/src/views/components/VideoStream.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="avionics">
+    <h1 class="display-1">Avionics Reporting</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'VideoStream',
+  data: function () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
