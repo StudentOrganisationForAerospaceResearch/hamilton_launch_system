cd5be5b33c05189f92e87e5bdade9623c1e81157
Byron Seto <byronseto@hotmail.com>
2018-04-25 23:55:06 -0600

    trying to get background process to work

--- hamilton_launch_livestream/dockerize.sh
+++ hamilton_launch_livestream/dockerize.sh
@@ -2,15 +2,47 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE_NAME="soar/hamilton_launch_livestream"
 DOCKER_HOME="/hamilton_launch_livestream/"
 
+
+test_count() {
+    local num_devices=`ls /dev/ | grep "video" | wc -l`
+    local device_string=""
+    local ffmpeg_command=""
+
+    docker run -d --rm \
+        --volume "$DIR:$DOCKER_HOME" \
+        -p 8090:8090 \
+        $IMAGE_NAME ffserver -f ffserver.conf
+
+    for (( i=0; i<=$((num_devices-1)); i++)); do
+        device_string=$device_string"--device=/dev/video$((i)) "
+        #ffmpeg_command=$ffmpeg_command"ffmpeg -f video4linux2 -s 640x480 -r 30 \
+            #-input_format mjpeg -i /dev/video$((i)) http://localhost:8090/feed$((i+1)).ffm \
+             #-nostdin -nostats"
+        #${ffmpeg_command}
+
+        docker run --rm \
+            --volume "$DIR:$DOCKER_HOME" \
+            -p 8090:809$((i)) \
+            --device=/dev/video$((i)) \
+            $IMAGE_NAME ffmpeg -f video4linux2 -s 640x480 -r 30 \
+                -input_format mjpeg \
+                -i /dev/video$((i)) \
+                http://localhost:8090/feed$((i+1)).ffm
+    done
+}
+
+
 if [ "$1" == "init" ]; then
     docker build -t $IMAGE_NAME .
     docker run --rm \
         --volume "$DIR:$DOCKER_HOME" \
-        $IMAGE_NAME ffserver
-elif [ "$1" == "test" ]; then
+        $IMAGE_NAME ffserver --help
+elif [ "$1" == "test-count" ]; then
+    test_count
+elif [ "$1" == "ffserver" ]; then
     docker run --rm \
         --volume "$DIR:$DOCKER_HOME" \
-        $IMAGE_NAME ls -al
+        $IMAGE_NAME ffserver --help
 elif [ "$1" == "run" ]; then
     docker run --rm \
         --volume "$DIR:$DOCKER_HOME" 
--- hamilton_launch_livestream/ffmpeg_command.sh
+++ hamilton_launch_livestream/ffmpeg_command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-ffmpeg -f video4linux2 -s 640x480 -r 30 -i /dev/video0 http://localhost:8090/feed1.ffm
+ffmpeg -f video4linux2 -s 640x480 -input_format mjpeg -r 30 -i /dev/video0 http://localhost:8090/feed1.ffm
 
