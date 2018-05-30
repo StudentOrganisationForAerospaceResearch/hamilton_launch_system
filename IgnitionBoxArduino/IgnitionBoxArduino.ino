93d3bcbea0a644fcacadd9137311de2bb9a6a50c
protocorbulo <adhamilt@ualberta.ca>
2018-05-30 02:03:27 -0600

    implimented check for arm and fire

--- IgnitionBoxArduino/IgnitionBoxArduino.ino
+++ IgnitionBoxArduino/IgnitionBoxArduino.ino
@@ -13,7 +13,35 @@
 #define USB_SERIAL_BAUD 115200
 #define UMB_SERIAL_BAUD 115200
 
-SoftwareSerial umbilical(SERIAL_RX, SERIAL_TX);  
+#define ARM_RELAY RELAY_1
+#define FIRE_RELAY RELAY_2
+
+SoftwareSerial umbilical(SERIAL_RX, SERIAL_TX);
+
+bool armed = false;
+bool fired  = false;
+
+
+void arm(){
+  digitalWrite(ARM_RELAY, HIGH);
+  armed = true;
+  umbilical.read();
+}
+
+void fire(){
+  if(armed){
+    digitalWrite(FIRE_RELAY,HIGH);
+    fired = true;
+    umbilical.read();
+  }
+}
+
+void abort(){
+  digitalWrite(FIRE_RELAY,LOW);
+  digitalWrite(ARM_RELAY,LOW);
+  armed = false;
+}
+
 
 void setup() {
   
@@ -39,6 +67,13 @@ void setup() {
   //Initialize the Umbilical Serial Connection
   umbilical.begin(UMB_SERIAL_BAUD);
 
+
+  digitalWrite(RELAY_1, LOW);
+  digitalWrite(RELAY_2, LOW);
+  digitalWrite(RELAY_3, LOW);
+  digitalWrite(RELAY_4, LOW);
+  digitalWrite(RELAY_5, LOW);
+  digitalWrite(RELAY_6, LOW);   
 }
 
 
@@ -52,8 +87,12 @@ void loop() {
     }
   }
   
-  if(Serial.avialable()>0){
-    while(Serial.available()>0){
+  if(Serial.available()>0){
+    byte header = umbilical.read();
+    if(header == 0x21) arm();
+    else if(header == 0x20) fire();
+    else if(header == 0x2F) abort();
+    else while(Serial.available()>0){
       byte data = Serial.read();
       umbilical.write(data);
     }
