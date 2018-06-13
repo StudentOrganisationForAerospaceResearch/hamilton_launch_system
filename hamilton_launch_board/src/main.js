4a2a4ef40cf78994cbdc71306cea11f038db67c4
Enoch Tsang <etsang1@hotmail.com>
2018-06-13 11:13:37 -0600

    infinite reconnections of webscoket

--- hamilton_launch_board/src/main.js
+++ hamilton_launch_board/src/main.js
@@ -13,7 +13,6 @@ Vue.use(VueNativeSock, `ws://${document.location.hostname}:8000/ws`, {
   store: store,
   format: 'json',
   reconnection: true, // (Boolean) whether to reconnect automatically (false)
-  reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
   reconnectionDelay: 3000 // (Number) how long to initially wait before attempting a new (1000)
 })
 
