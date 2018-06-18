1abfa6065d7f433ee0cd626603ea9cdcad9b610f
Enoch Tsang <etsang1@hotmail.com>
2018-06-17 23:57:01 -0600

    remove some code used for testing

--- hamilton_launch_board/src/views/pages/VideoFeed.vue
+++ hamilton_launch_board/src/views/pages/VideoFeed.vue
@@ -30,7 +30,6 @@ export default {
   },
   methods: {
     isMobile: function () {
-      return false
       return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     }
   },
